c9ceaa5ed9e6d8364ecf4399f27df7cadd1e06f7
JorgeinArmor <jorgegalvez1694@gmail.com>
2024-03-20 12:52:45 +0000

    cmake

--- src/performance.cpp
+++ src/performance.cpp
@@ -1,9 +1,32 @@
 #include <cstdlib>
+#include <iomanip>
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
+#ifdef HAVE_HIP
 #include <hip/hip_runtime.h>
 #include <hipblas/hipblas.h>
+#elif HAVE_CUDA
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#define hipblasOperation_t cublasOperation_t
+#define HIPBLAS_OP_T CUBLAS_OP_T
+#define HIPBLAS_OP_N CUBLAS_OP_N
+#define hipMalloc cudaMalloc
+#define hipHostMalloc cudaMallocHost
+#define hipStream_t cudaStream_t
+#define hipStreamCreate cudaStreamCreate
+#define hipblasHandle_t cublaHandle_t
+#define hipblasStatus_t cublasStatus_t 
+#define hipblasCreate cublasCreate
+#define hipblasSetStream cublasSetStream
+#define hipMemcpy cudaMemcpy
+#define hipMemcpyHostToDevice cudaMemcpyHostToDevice
+#define hipblasDgemm cublasDgemm
+#define HIPBLAS_STATUS_SUCESS CUBLAS_STATUS_SUCESS
+#define hipDeviceSynchronize cudaDeviceSynchronize
+#endif 
 #include <chrono>
 // originally designed and programmed for nvidia gpus
 // by Calum Snowdon at the Australian National Univesrity 
@@ -13,6 +36,13 @@
 
 int main(int argc, char *argv[]) {
   using namespace std::chrono;
+
+    std::cout << "              __\n";
+    std::cout << "             / _)\n";
+    std::cout << "    _.----._/ /\n";
+    std::cout << "  /          /\n";
+    std::cout << "__/ (  | (  |\n";
+    std::cout << "/__.-'|_|--|_|\n";
 
   // Parse args
   if (argc != 8) {
@@ -47,7 +77,7 @@ const int reps_of_reps = atoi(argv[7]);
   double *HA, *HB;
   hipHostMalloc((void**)&HA, m*k*sizeof(double));
   hipHostMalloc((void**) &HB, k*n*sizeof(double));
-srand(time(NULL));
+	srand(time(NULL));
   for (int i=0; i<m*k; i++)
     HA[i] = ((double)rand() / RAND_MAX);
   for (int i=0; i<k*n; i++)
@@ -78,7 +108,8 @@ srand(time(NULL));
 
   // Do the computation: schedule [reps] dgemm's using the same buffers 
   // and stream, then synchronize.
-  std::cout << "Performing " << reps << " repetitions of " << m << "*" << k << " by " << k << "*" << n << std::endl;
+  std::cout << "Performing " << reps << " repetitions of " << m << "*" << k << " by " << k << "*" << n << " " << reps_of_reps << " times " << std::endl;
+std::cout << std::left << std::setw(12) << "Time (us)" << std::setw(12) << "GFLOP/s" << std::setw(4) << "Rep" << std::endl;
 	for(int j = 0; j < reps_of_reps; ++j){
   auto start = high_resolution_clock::now();
   for (int i=0; i<reps; i++) {
@@ -92,8 +123,11 @@ srand(time(NULL));
   auto end = high_resolution_clock::now();
 
   auto duration = duration_cast<microseconds>(end-start);
-  std::cout << "Time     " << duration.count() << "us" << std::endl;
-  std::cout << "GFLOP/s: " << ((double)2*m*k*n*reps)/((double)duration.count())/1000 << " in iter " << j << std::endl;
+    double gflops = ((double)2 * m * k * n * reps) / duration.count() / 1000;
+
+    std::cout << std::left << std::setw(12) << duration.count()
+              << std::setw(12) << std::setprecision(6) << gflops
+              << std::setw(4) << j << std::endl;
 }
   return 0;
 }
