840e12df640a8eba553ddf5a5759080f39377796
JorgeinArmor <jorgegalvez1694@gmail.com>
2024-03-20 13:02:39 +0000

    moved the grossness somewhere else

--- src/performance.cpp
+++ src/performance.cpp
@@ -3,30 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#ifdef HAVE_HIP
-#include <hip/hip_runtime.h>
-#include <hipblas/hipblas.h>
-#elif HAVE_CUDA
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cublas_v2.h>
-#define hipblasOperation_t cublasOperation_t
-#define HIPBLAS_OP_T CUBLAS_OP_T
-#define HIPBLAS_OP_N CUBLAS_OP_N
-#define hipMalloc cudaMalloc
-#define hipHostMalloc cudaMallocHost
-#define hipStream_t cudaStream_t
-#define hipStreamCreate cudaStreamCreate
-#define hipblasHandle_t cublasHandle_t
-#define hipblasStatus_t cublasStatus_t 
-#define hipblasCreate cublasCreate
-#define hipblasSetStream cublasSetStream
-#define hipMemcpy cudaMemcpy
-#define hipMemcpyHostToDevice cudaMemcpyHostToDevice
-#define hipblasDgemm cublasDgemm
-#define HIPBLAS_STATUS_SUCCESS CUBLAS_STATUS_SUCCESS
-#define hipDeviceSynchronize cudaDeviceSynchronize
-#endif 
+#include "gpu.hpp"
 #include <chrono>
 // originally designed and programmed for nvidia gpus
 // by Calum Snowdon at the Australian National Univesrity 
